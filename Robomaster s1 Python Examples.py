8041ce5c82e7a3d715f0cddc7023f728e7c6d6cd
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-07-08 01:13:40 -0400

    Update Robomaster s1 Python Examples.py

--- Robomaster s1 Python Examples.py
+++ Robomaster s1 Python Examples.py
@@ -9,7 +9,6 @@ while True:
     led_ctrl.set_single_led(rm_define.armor_top_left,[1,3,5,7],rm_define.effect_always_on)
     led_ctrl.set_bottom_led(rm_define.armor_bottom_all,255,0,255,rm_define.effect_always_on)
     time.sleep(.095)
-
     led_ctrl.set_top_led(rm_define.armor_top_right,0,255,255,rm_define.effect_always_off)
     led_ctrl.set_single_led(rm_define.armor_top_right,[2,4,6,8],rm_define.effect_always_on)
     led_ctrl.set_top_led(rm_define.armor_top_left,255,0,255,rm_define.effect_always_off)
