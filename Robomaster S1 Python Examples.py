cac7e95df3ffe4e2e88dc7fdee48a9e7df27fdbe
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2020-08-25 06:22:33 -0400

    Update Robomaster S1 Python Examples.py

--- Robomaster S1 Python Examples.py
+++ Robomaster S1 Python Examples.py
@@ -676,6 +676,65 @@ def start():
             
 '''----------------------------------------------------------------------------------------------------------------'''
 
+# Make Robomaster fire his blaster every time he sees a person.
+# Set the zoom value update from 1 to 4 for farther distances.
+
+def start():
+
+    led1,led2=(0,255)
+    blink_rate=(6,8)
+
+# Blaster Fire Example Function:
+
+    def blaster_fire_example():
+
+        robot_ctrl.set_mode(rm_define.robot_mode_free)
+        media_ctrl.zoom_value_update(1)
+        
+        while True:
+            led_ctrl.set_top_led(rm_define.armor_top_all,led2,led2,led1,rm_define.effect_breath)
+            led_ctrl.set_bottom_led(rm_define.armor_bottom_all,led2,led2,1,rm_define.effect_breath)
+
+            vision_ctrl.enable_detection(rm_define.vision_detection_people)
+            vision_ctrl.cond_wait(rm_define.cond_recognized_people)
+            
+            while True:
+                randgimbal_speed=random.randint(20,100)
+                randup=random.randint(1,55)
+
+                gimbal_ctrl.set_rotate_speed(randgimbal_speed)
+                media_ctrl.play_sound(rm_define.media_sound_gimbal_rotate,wait_for_complete_flag=False)
+
+                led_ctrl.set_flash(rm_define.armor_all,blink_rate[0])
+                led_ctrl.set_top_led(rm_define.armor_top_all,led2,led1,led1,rm_define.effect_marquee)
+                led_ctrl.set_bottom_led(rm_define.armor_bottom_all,led2,led1,led1,rm_define.effect_flash)
+
+                gimbal_ctrl.rotate_with_degree(rm_define.gimbal_up,randup)
+
+                led_ctrl.set_flash(rm_define.armor_all,blink_rate[1])
+                led_ctrl.set_top_led(rm_define.armor_top_all,led1,led2,led2,rm_define.effect_flash)
+                led_ctrl.set_bottom_led(rm_define.armor_bottom_all,led1,led2,led2,rm_define.effect_flash)
+
+                media_ctrl.play_sound(rm_define.media_sound_shoot,wait_for_complete_flag=True)
+                media_ctrl.play_sound(rm_define.media_sound_shoot,wait_for_complete_flag=True)
+
+                commands_exit=random.randint(1,2)
+                if commands_exit==1:
+                    continue
+                elif commands_exit==2:
+                    led_ctrl.set_flash(rm_define.armor_all,blink_rate[0])
+                    led_ctrl.set_top_led(rm_define.armor_top_all,led2,led1,led1,rm_define.effect_marquee)
+                    led_ctrl.set_bottom_led(rm_define.armor_bottom_all,led2,led1,led1,rm_define.effect_flash)
+                    gimbal_ctrl.recenter()                        
+                    break
+                
+# Call up the blaster_fire_example function.
+# Be sure to properly indent the function or it won't work.
+
+    blaster_fire_example()
+            
+'''----------------------------------------------------------------------------------------------------------------'''
+
 # More future Robomaster s1 Python examples still to come as I learn more and more, each and every day.
 
 '''----------------------------------------------------------------------------------------------------------------'''
