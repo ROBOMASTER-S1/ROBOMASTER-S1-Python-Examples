74a194a7995613ebb8709e654b622e463d4eaa4d
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2020-09-08 17:17:17 -0400

    Update Robomaster S1 Python Commands.py

--- Robomaster S1 Python Commands.py
+++ Robomaster S1 Python Commands.py
@@ -756,7 +756,9 @@ if vision_ctrl.check_condition(rm_define.cond_recognized_pose_give_in):
 
 if vision_ctrl.check_condition(rm_define.cond_recognized_pose_capture):
     pass
-
+                              
+'''---------------------------------------------------------------------------------------------------------------------------------------------'''
+                              
 if media_ctrl.check_condition(rm_define.cond_sound_recognized_applause_twice) :
         pass
 
