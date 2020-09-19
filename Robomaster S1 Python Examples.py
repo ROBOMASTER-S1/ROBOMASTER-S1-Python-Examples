be8b40d2c8a128259c3a4079ea6589a3883d5f2b
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2020-09-18 20:52:09 -0400

    Update Robomaster S1 Python Examples.py

--- Robomaster S1 Python Examples.py
+++ Robomaster S1 Python Examples.py
@@ -1379,6 +1379,52 @@ def sound_recognized_applause_thrice(msg):
 
 '''----------------------------------------------------------------------------------------------------------------------------------------------------'''
 
+# below is the very same program example as the one above, the only difference is there are no 'def'
+# colour function statements. Type and execute/run the program example and see what happens.
+
+media=media_ctrl
+led=led_ctrl
+define=rm_define
+
+l1,l2=0,255
+seconds=1
+
+def start():
+
+    media.enable_sound_recognition(rm_define.sound_detection_applause)
+    led.turn_off(define.armor_all)
+
+    while True:
+
+        media.cond_wait(define.cond_sound_recognized_applause_twice)
+        led.set_top_led(define.armor_top_all,l2,l1,l1,define.effect_always_on)
+        led.set_bottom_led(define.armor_bottom_all,l2,l1,l1,define.effect_always_on)
+
+        media.cond_wait(define.cond_sound_recognized_applause_twice)
+        led.set_top_led(define.armor_top_all,l2,l2,l1,define.effect_always_on)
+        led.set_bottom_led(define.armor_bottom_all,l2,l2,l1,define.effect_always_on)
+
+        media.cond_wait(define.cond_sound_recognized_applause_twice)
+        led.set_top_led(define.armor_top_all,l1,l1,l2,define.effect_always_on)
+        led.set_bottom_led(define.armor_bottom_all,l1,l1,l2,define.effect_always_on)
+
+        media.cond_wait(define.cond_sound_recognized_applause_twice)
+        led.set_top_led(define.armor_top_all,l1,l2,l1,define.effect_always_on)
+        led.set_bottom_led(define.armor_bottom_all,l1,l2,l1,define.effect_always_on)
+
+        media.cond_wait(define.cond_sound_recognized_applause_twice)
+        led.set_top_led(define.armor_top_all,l2,l1,l2,define.effect_always_on)
+        led.set_bottom_led(define.armor_bottom_all,l2,l1,l2,define.effect_always_on)
+
+        media.cond_wait(define.cond_sound_recognized_applause_twice)
+        led.set_top_led(define.armor_top_all,l1,l2,l2,define.effect_always_on)
+        led.set_bottom_led(define.armor_bottom_all,l1,l2,l2,define.effect_always_on)
+
+def sound_recognized_applause_thrice(msg):
+    led.turn_off(define.armor_all)
+    
+'''----------------------------------------------------------------------------------------------------------------------------------------------------'''
+
 # More future Robomaster s1 Python examples still to come as I learn more and more, each and every day.
 
 '''----------------------------------------------------------------------------------------------------------------------------------------------------'''
