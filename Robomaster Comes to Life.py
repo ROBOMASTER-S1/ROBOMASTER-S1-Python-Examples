22147dba1015c1c73e8f791f4ca4ad16efaa8a94
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2021-01-07 11:51:57 -0500

    Update Robomaster Comes to Life.py

--- Robomaster Comes to Life.py
+++ Robomaster Comes to Life.py
@@ -62,7 +62,8 @@ def start():
 # Robot Start:
 
     def robot_start():
-                 
+
+        led.gun_led_off()                 
         led.set_top_led(define.armor_top_all,led2,led1,led1,define.effect_breath)
         led.set_bottom_led(define.armor_bottom_all,led2,led1,led1,define.effect_breath)
         gimbal.recenter()
@@ -86,6 +87,7 @@ def start():
             while True:
                 randspeed=random.choice(rotate_speed)
                 randrotate=random.randint(1,90)
+                commands_exit=random.randint(1,2)
 
                 if randspeed==20:
                     led.set_flash(define.armor_all,blink_rate[0])
@@ -108,11 +110,8 @@ def start():
 
                     gimbal.set_rotate_speed(randspeed)
                     gimbal.rotate_with_degree(define.gimbal_right,randrotate)
-
-                    commands_exit=random.randint(1,2)
                     
                     if commands_exit==1:
-                        robot.set_mode(define.robot_mode_chassis_follow)
                         continue
                     elif commands_exit==2:
                         robot.set_mode(define.robot_mode_chassis_follow)
@@ -127,6 +126,7 @@ def start():
             while True:
                 randspeed=random.choice(rotate_speed)
                 randrotate=random.randint(1,90)
+                commands_exit=random.randint(1,2)
 
                 if randspeed==20:
                     led.set_flash(define.armor_all,blink_rate[0])
@@ -149,11 +149,8 @@ def start():
 
                     gimbal.set_rotate_speed(randspeed)
                     gimbal.rotate_with_degree(define.chassis_left,randrotate)
-
-                    commands_exit=random.randint(1,2)
                     
                     if commands_exit==1:
-                        robot.set_mode(define.robot_mode_chassis_follow)
                         continue
                     elif commands_exit==2:
                         robot.set_mode(define.robot_mode_chassis_follow)
@@ -172,6 +169,7 @@ def start():
             while True:
                 randspeed=random.choice(rotate_speed)
                 randrotate=random.randint(1,90)
+                commands_exit=random.randint(1,2)
 
                 if randspeed==20:
                     led.set_flash(define.armor_all,blink_rate[0])
@@ -194,11 +192,8 @@ def start():
 
                     chassis.set_rotate_speed(randspeed)
                     chassis.rotate_with_degree(define.clockwise,randrotate)
-
-                    commands_exit=random.randint(1,2)
                     
                     if commands_exit==1:
-                        robot.set_mode(define.robot_mode_chassis_follow)
                         continue
                     elif commands_exit==2:
                         robot.set_mode(define.robot_mode_chassis_follow)
@@ -213,6 +208,7 @@ def start():
             while True:
                 randspeed=random.choice(rotate_speed)
                 randrotate=random.randint(1,90)
+                commands_exit=random.randint(1,2)
 
                 if randspeed==20:
                     led.set_flash(define.armor_all,blink_rate[0])
@@ -235,11 +231,8 @@ def start():
 
                     chassis.set_rotate_speed(randspeed)
                     chassis.rotate_with_degree(define.anticlockwise,randrotate)
-
-                    commands_exit=random.randint(1,2)
                     
                     if commands_exit==1:
-                        robot.set_mode(define.robot_mode_chassis_follow)
                         continue
                     elif commands_exit==2:
                         robot.set_mode(define.robot_mode_chassis_follow)
@@ -259,6 +252,7 @@ def start():
                 randspeed=random.choice(rotate_speed)
                 randwheel=random.choice(wheel_degree)
                 randrotate=random.randint(1,180)
+                commands_exit=random.randint(1,2)
 
                 if randspeed==20:
                     led.set_flash(define.armor_all,blink_rate[0])
@@ -290,11 +284,8 @@ def start():
 
                     chassis.set_trans_speed(drive_speed)
                     chassis.move_with_time(randwheel,seconds)
-
-                    commands_exit=random.randint(1,2)
                     
                     if commands_exit==1:
-                        robot.set_mode(define.robot_mode_chassis_follow)
                         continue
                     elif commands_exit==2:
                         robot.set_mode(define.robot_mode_chassis_follow)
@@ -310,6 +301,7 @@ def start():
                 randspeed=random.choice(rotate_speed)
                 randwheel=random.choice(wheel_degree)
                 randrotate=random.randint(1,180)
+                commands_exit=random.randint(1,2)
 
                 if randspeed==20:
                     led.set_flash(define.armor_all,blink_rate[0])
@@ -341,11 +333,8 @@ def start():
 
                     chassis.set_trans_speed(drive_speed)
                     chassis.move_with_time(randwheel,seconds)
-
-                    commands_exit=random.randint(1,2)
                     
                     if commands_exit==1:
-                        robot.set_mode(define.robot_mode_chassis_follow)
                         continue
                     elif commands_exit==2:
                         robot.set_mode(define.robot_mode_chassis_follow)
@@ -370,6 +359,7 @@ def start():
                 randgimbal_speed=random.randint(20,100)
                 randrotate=random.randint(1,250)
                 randangle=random.randint(-15,35)
+                commands_exit=random.randint(1,2)
 
                 gimbal.set_rotate_speed(randgimbal_speed)
                 media.play_sound(define.media_sound_gimbal_rotate,wait_for_complete_flag=False)
@@ -377,8 +367,6 @@ def start():
                 gimbal.set_rotate_speed(scan_speed)
                 media.play_sound(define.media_sound_scanning,wait_for_complete_flag=False)
                 gimbal.angle_ctrl(randrotate,randangle)
-
-                commands_exit=random.randint(1,2)
                 
                 if commands_exit==1:
                     continue
@@ -402,6 +390,7 @@ def start():
                 randgimbal_speed=random.randint(20,100)
                 randrotate=random.randint(-250,1)
                 randangle=random.randint(-15,35)
+                commands_exit=random.randint(1,2)
 
                 gimbal.set_rotate_speed(randgimbal_speed)
                 media.play_sound(define.media_sound_gimbal_rotate,wait_for_complete_flag=False)
@@ -409,8 +398,6 @@ def start():
                 gimbal.set_rotate_speed(scan_speed)
                 media.play_sound(define.media_sound_scanning,wait_for_complete_flag=False)
                 gimbal.angle_ctrl(randrotate,randangle)
-
-                commands_exit=random.randint(1,2)
                 
                 if commands_exit==1:
                     continue
@@ -430,6 +417,7 @@ def start():
         while True:
             randgimbal_speed=random.randint(20,100)
             randup=random.randint(1,55)
+            commands_exit=random.randint(1,2)
 
             gimbal.set_rotate_speed(randgimbal_speed)
             media.play_sound(define.media_sound_gimbal_rotate,wait_for_complete_flag=False)
@@ -446,8 +434,6 @@ def start():
 
             media.play_sound(define.media_sound_shoot,wait_for_complete_flag=True)
             media.play_sound(define.media_sound_shoot,wait_for_complete_flag=True)
-
-            commands_exit=random.randint(1,2)
             
             if commands_exit==1:
                 continue
@@ -466,6 +452,7 @@ def start():
 
         while True:
             randdelay=random.randint(1,4)
+            commands_exit=random.randint(1,3)
 
             led.set_top_led(define.armor_top_all,led2,led2,led1,define.effect_breath)
             led.set_bottom_led(define.armor_bottom_right,led2,led2,led1,define.effect_breath)
@@ -474,8 +461,6 @@ def start():
             led.set_bottom_led(define.armor_bottom_back,led2,led1,led1,define.effect_breath)
             gimbal.recenter()
             time.sleep(randdelay)
-
-            commands_exit=random.randint(1,3)
             
             if commands_exit==1:
                 continue
@@ -487,29 +472,30 @@ def start():
     while True:
         randfunc=random.randint(1,5)
         randloop=random.randint(1,4)
-        randscan=random.randint(1,30)       
-
+        randscan=random.randint(1,30)
+        
         if randfunc==1:
             for i in range(randloop):                    
                 chassis_follow_gimbal()
 
-        elif randfunc==2:                
+        elif randfunc==2:
             gimbal_free_mode()
 
-        elif randfunc==3:                
+        elif randfunc==3:
             chassis_free_mode()
 
-        elif randfunc==4:                
+        elif randfunc==4:
             blaster_fire()
 
-        elif randfunc==5:                
+        elif randfunc==5:
             sleep_mode()
 
-        if randscan==15:                
+        if randscan==15:
             scan_search()
 
 def sound_recognized_applause_twice(msg):
-
+    
+    led.gun_led_off()
     led.set_top_led(define.armor_top_all,led2,led1,led1,define.effect_breath)
     led.set_bottom_led(define.armor_bottom_all,led2,led1,led1,define.effect_breath)
     gimbal.recenter()
