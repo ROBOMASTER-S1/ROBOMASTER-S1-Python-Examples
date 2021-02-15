edad45b762bc68ff5c0b06f899e9b1dfa7ac5469
Joseph C. Richardson <34896540+ROBOMASTER-S1@users.noreply.github.com>
2021-02-14 20:06:17 -0500

    Update Clap Disco Lamp.py

--- Clap Disco Lamp.py
+++ Clap Disco Lamp.py
@@ -1,6 +1,6 @@
-# Make the Robomaster S1 become a cool Clap Disco Lamp. Simply clap your hands twice every time to change his
-# leds into your favourite Disco leds setting. Clap your hands three times to turn his leds off and on, while
-# maintaining your desired leds settings
+# Make the Robomaster S1 become a cool Clap Disco Lamp. Simply clap your hands twice every time to change
+# his leds into your favourite Disco leds setting. Clap your hands three times to turn his leds off and on,
+# while maintaining your desired leds settings.
 
 led=led_ctrl
 media=media_ctrl
