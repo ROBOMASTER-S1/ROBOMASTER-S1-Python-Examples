e8b9e3168868311899f0868779b9a91bba86cfc4
Joseph C. Richardson <josephcrichardson08@gmail.com>
2021-08-02 03:27:13 -0400

    Update Robomaster S1 Comes to Life.py

--- Robomaster S1 Comes to Life.py
+++ Robomaster S1 Comes to Life.py
@@ -1,4 +1,4 @@
-'''Robomaster S1 Comes to Life upgrade 3.0'''
+'''Robomaster S1 Comes to Life 4.0'''
 
 # Make Robomaster S1 come to life. Simply copy or download this program example.
 
@@ -13,36 +13,38 @@
 # To avoid damaging your Robomaster S1, never set any speeds higher than they
 # are shown here, especially in smaller play areas. Note: be cautious when setting
 # the drive_speed variable higher than 0.2 and the seconds variable, who's default
-# is 1 second per drive time distance.
+# is 2 seconds per drive time distance.
 
 # IMPORTANT! Never pick up or move the Robomaster S1, while its program is
 # running. Doing so may cause damage to the unit; you must stop the program first.
 
-# Robomaster S1 features illustrated here are as follows:
+# Robomaster S1 feature functions illustrated here are as follows:
 
-# Robomster S1 Start
-# Robot All Wheel Omn directional Drive
-# Gimbal Free Mode Right Left
-# Chassis Follow Gimbal Right Left
-# Blaster Fire with sound effect
-# Scan Search Right Left with Sound Effect
-# Single Led Chassis Follow Gimbal Rotation Right Left
-# Double Led Chassis Follow Gimbal Rotation Right Left
-# Quad Led Gimbal Rotation Up Down
-# RGB Colour Trail Chasers Forward Reverse
-# RGB Flash Colour Changers Forward Reverse
-# Sleep Mode
+# Robomster S1 Start Function
+# Robot All Wheel Omn directional Drive Function
+# Gimbal Free Mode Right Left Function
+# Chassis Follow Gimbal Right Left Function
+# Dance Rock Function
+# Blaster Fire Function with sound effect
+# Scan Search Right Left Function with sound effect
+# Sleep Function
+# Single Led Chassis Follow Gimbal Rotation Right Left Function
+# Double Led Chassis Follow Gimbal Rotation Right Left Function
+# Quad Led Gimbal Rotation Up Down Function with sound effect
+# RGB Colour Trail Chasers Forward Reverse Function
+# RGB Colour Changers Forward Reverse Function
+# RGB Flash Colour Changers Function
+
+# User Interaction Features:
+
 # Detection Applause
 # Hit Detection
 
-# Note: Chassis Free Mode and Gimbal Follow Chassis features have been removed
-# from this Robomaster S1 Python program example. However, I have beeen adding
-# more Robomaster S1 features, such as my new RGB Chaser Lights features.
-
-# I sure hope other Robomaster people find this program very enjoyable and useful. I
-# sure put a lot of time into creating it. It sure wasn't no cakewalk when the gimbal
-# and chassis would conflict each other during practice execution/runs before I finally
-# figured it all out.
+# I sure hope other Robomaster S1 people find this program very
+# enjoyable and useful. I sure put a lot of time into creating it.
+# It sure wasn't no cakewalk when the gimbal and chassis would conflict
+# each other during practice execution/runs before I finally figured it
+# all out. So please enjoy...
 
 robot=robot_ctrl
 gimbal=gimbal_ctrl
@@ -53,11 +55,11 @@ led=led_ctrl
 define=rm_define
 
 wheel_degree=0,180,90,-90,45,-45,135,-135
-second,delay=1,.1;delay1,delay2=.1,1
-l1,l2=0,255;blink_rate=2,3,4,5,6
+l1,l2=0,255;blink_rate=2,3,4,5,6,8
 scan_speed=40;drive_speed=.2
-rotate_speed=20,30,40,50,60
+rotate_speed=20,30,40,50,60,80
 seconds,milli_seconds=2,.1
+delay1,delay2=1,.1
 a,b,c=1,2,3
 
 RGB_RY=[
@@ -99,7 +101,7 @@ def start():
     media.enable_sound_recognition(define.sound_detection_applause)
     armor.set_hit_sensitivity(10)
 
-# Robomster S1 Start:
+# Robomster S1 Start Function:
    
     def robomaster_s1_start():
         led.gun_led_off()
@@ -115,7 +117,7 @@ def start():
         media.play_sound(define.media_sound_count_down,wait_for_complete_flag=True)
         gimbal.recenter()
 
-# Robot All Wheel Omni Directional Drive:
+# Robot All Wheel Omni Directional Drive Function:
 
     def robot_all_wheel_omni_directional_drive():
 
@@ -133,6 +135,7 @@ def start():
                 elif randspeed==40:led.set_flash(define.armor_all,blink_rate[2])
                 elif randspeed==50:led.set_flash(define.armor_all,blink_rate[3])
                 elif randspeed==60:led.set_flash(define.armor_all,blink_rate[4])
+                elif randspeed==60:led.set_flash(define.armor_all,blink_rate[5])
 
                 if randrotate<=180:
                     led.set_top_led(define.armor_top_left,l2,l1,l1,define.effect_always_on)
@@ -182,6 +185,7 @@ def start():
                 elif randspeed==40:led.set_flash(define.armor_all,blink_rate[2])
                 elif randspeed==50:led.set_flash(define.armor_all,blink_rate[3])
                 elif randspeed==60:led.set_flash(define.armor_all,blink_rate[4])
+                elif randspeed==60:led.set_flash(define.armor_all,blink_rate[5])
 
                 if randrotate<=180:
                     led.set_top_led(define.armor_top_right,l2,l1,l1,define.effect_always_on)
@@ -217,7 +221,7 @@ def start():
 
         robot_all_wheel_omni_directional_drive_left()
 
-# Gimbal Free Mode Right Left:
+# Gimbal Free Mode Right Left Function:
 
     def gimbal_free_mode_right_left():
 
@@ -234,6 +238,7 @@ def start():
                 elif randspeed==40:led.set_flash(define.armor_all,blink_rate[2])
                 elif randspeed==50:led.set_flash(define.armor_all,blink_rate[3])
                 elif randspeed==60:led.set_flash(define.armor_all,blink_rate[4])
+                elif randspeed==60:led.set_flash(define.armor_all,blink_rate[5])
 
                 if randrotate<=90:
                     led.set_top_led(define.armor_top_left,l2,l1,l1,define.effect_always_on)
@@ -264,6 +269,7 @@ def start():
                 elif randspeed==40:led.set_flash(define.armor_all,blink_rate[2])
                 elif randspeed==50:led.set_flash(define.armor_all,blink_rate[3])
                 elif randspeed==60:led.set_flash(define.armor_all,blink_rate[4])
+                elif randspeed==60:led.set_flash(define.armor_all,blink_rate[5])
 
                 if randrotate<=90:
                     led.set_top_led(define.armor_top_right,l2,l1,l1,define.effect_always_on)
@@ -281,7 +287,7 @@ def start():
 
         gimbal_free_mode_left()
 
-# Chassis Follow Gimbal Right Left:
+# Chassis Follow Gimbal Right Left Function:
 
     def chassis_follow_gimbal_right_left():
 
@@ -298,6 +304,7 @@ def start():
                 elif randspeed==40:led.set_flash(define.armor_all,blink_rate[2])
                 elif randspeed==50:led.set_flash(define.armor_all,blink_rate[3])
                 elif randspeed==60:led.set_flash(define.armor_all,blink_rate[4])
+                elif randspeed==60:led.set_flash(define.armor_all,blink_rate[5])
 
                 if randrotate<=90:
                     led.set_top_led(define.armor_top_left,l2,l1,l1,define.effect_always_on)
@@ -328,6 +335,7 @@ def start():
                 elif randspeed==40:led.set_flash(define.armor_all,blink_rate[2])
                 elif randspeed==50:led.set_flash(define.armor_all,blink_rate[3])
                 elif randspeed==60:led.set_flash(define.armor_all,blink_rate[4])
+                elif randspeed==60:led.set_flash(define.armor_all,blink_rate[5])
 
                 if randrotate<=90:
                     led.set_top_led(define.armor_top_right,l2,l1,l1,define.effect_always_on)
@@ -345,7 +353,32 @@ def start():
 
         chassis_follow_gimbal_left()
 
-# Blaster Fire:
+# Dance Rock Function:
+
+    def dance_rock():
+
+        robot.set_mode(define.robot_mode_free)
+        
+        chassis.set_rotate_speed(120)
+        gimbal.set_rotate_speed(120)
+
+        led.set_flash(define.armor_all,4)
+        led.set_top_led(define.armor_top_all,l1,l2,l1,define.effect_marquee)
+        led.set_bottom_led(define.armor_bottom_all,l1,l2,l1,define.effect_flash)
+        led.gun_led_on()
+
+        for i in range(2):
+            gimbal.rotate(define.gimbal_right)
+            chassis.rotate_with_time(define.anticlockwise, 0.3)
+            gimbal.rotate(define.gimbal_left)
+            chassis.rotate_with_time(define.clockwise, 0.6)
+            gimbal.rotate(define.gimbal_right)
+            chassis.rotate_with_time(define.anticlockwise, 0.3)
+    
+        led.gun_led_off()
+        gimbal.recenter()
+
+# Blaster Fire Function:
 
     def blaster_fire():
         robot.set_mode(define.robot_mode_free)
@@ -378,9 +411,10 @@ def start():
                 led.set_flash(define.armor_all,6)
                 led.set_top_led(define.armor_top_all,l2,l1,l1,define.effect_marquee)
                 led.set_bottom_led(define.armor_bottom_all,l2,l1,l1,define.effect_flash)
+                media.play_sound(define.media_sound_gimbal_rotate,wait_for_complete_flag=False)
                 gimbal.recenter();break
 
-# Scan Search Right Left :
+# Scan Search Right Left Function:
 
     def scan_search_right_left():
 
@@ -439,7 +473,26 @@ def start():
 
         scan_search_left()
 
-# Single Led Chassis Follow Gimbal Rotation Right Left:
+# Sleep Function:
+
+    def sleep():
+        robot.set_mode(define.robot_mode_free)
+
+        while True:
+            randdelay=random.randint(1,4)
+            commands_exit=random.randint(a,c)
+
+            led.set_top_led(define.armor_top_all,l2,l2,l1,define.effect_breath)
+            led.set_bottom_led(define.armor_bottom_right,l2,l2,l1,define.effect_breath)
+            led.set_bottom_led(define.armor_bottom_left,l2,l2,l1,define.effect_breath)
+            led.set_bottom_led(define.armor_bottom_front,l2,l2,l2,define.effect_breath)
+            led.set_bottom_led(define.armor_bottom_back,l2,l1,l1,define.effect_breath)
+            gimbal.recenter();time.sleep(randdelay)
+
+            if commands_exit==a:continue
+            elif commands_exit==b:break
+
+# Single Led Chassis Follow Gimbal Rotation Right Left Function:
 
     def single_led_chassis_follow_gimbal_rotation_right_left():
 
@@ -461,7 +514,7 @@ def start():
 
                     led.set_bottom_led(define.armor_bottom_all,
                     RGB1[-i][0],RGB1[-i][1],RGB1[-i][2],define.effect_always_on)
-                    time.sleep(delay);led.gun_led_off()
+                    time.sleep(delay2);led.gun_led_off()
                 x+=1
 
                 if commands_exit==a:continue
@@ -485,7 +538,7 @@ def start():
 
                     led.set_bottom_led(define.armor_bottom_all,
                     RGB1[-i][0],RGB1[-i][1],RGB1[-i][2],define.effect_always_on)
-                    time.sleep(delay);led.gun_led_off()
+                    time.sleep(delay2);led.gun_led_off()
                 x+=1
 
                 if commands_exit==a:continue
@@ -493,7 +546,7 @@ def start():
 
         single_led_chassis_follow_gimbal_rotation_right()
 
-# Double Led Chassis Follow Gimbal Rotation Right Left:
+# Double Led Chassis Follow Gimbal Rotation Right Left Function:
 
     def double_led_chassis_follow_gimbal_rotation_right_left():
 
@@ -514,7 +567,7 @@ def start():
 
                     led.set_bottom_led(define.armor_bottom_all,
                     RGB1[-i][0],RGB1[-i][1],RGB1[-i][2],define.effect_always_on)
-                    time.sleep(delay);led.gun_led_off()
+                    time.sleep(delay2);led.gun_led_off()
                 x+=1
 
                 if commands_exit==a:continue
@@ -539,7 +592,7 @@ def start():
 
                     led.set_bottom_led(define.armor_bottom_all,
                     RGB1[-i][0],RGB1[-i][1],RGB1[-i][2],define.effect_always_on)
-                    time.sleep(delay);led.gun_led_off()
+                    time.sleep(delay2);led.gun_led_off()
                 x+=1
 
                 if commands_exit==a:continue
@@ -547,7 +600,7 @@ def start():
 
         double_led_chassis_follow_gimbal_rotation_left()
 
-# Quad Led Gimbal Rotation Up Down:
+# Quad Led Gimbal Rotation Up Down Function:
 
     def quad_led_gimbal_rotation_up_down():
         robot.set_mode(define.robot_mode_free)
@@ -566,15 +619,16 @@ def start():
 
                 led.set_bottom_led(define.armor_bottom_all,
                 RGB_RY[-i][0],RGB_RY[-i][1],RGB_RY[-i][2],define.effect_always_on)
-                time.sleep(delay)
+                time.sleep(delay2)
             x+=1
 
         led.set_flash(define.armor_all,blink_rate[4])
         led.set_top_led(define.armor_top_all,l2,l1,l1,define.effect_flash)
         led.set_bottom_led(define.armor_bottom_all,l2,l1,l1,define.effect_flash)
+        media.play_sound(define.media_sound_gimbal_rotate,wait_for_complete_flag=False)
         gimbal.recenter();led.gun_led_off()
 
-# RGB Colour Trail Chasers Forward Reverse:
+# RGB Colour Trail Chasers Forward Reverse Function:
 
     def rgb_colour_trail_chasers_forward_reverse():
 
@@ -582,26 +636,26 @@ def start():
             robot.set_mode(define.robot_mode_chassis_follow)
             gimbal.set_rotate_speed(rotate_speed[2])
 
-            for i in range(1,8):
+            for j in range(1,8):
                 commands_exit=random.randint(a,b)
                 gimbal.rotate(define.gimbal_right)
                 led.gun_led_on()
                 led.set_top_led(define.armor_top_all,
-                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_off)
+                RGB2[j][0],RGB2[j][1],RGB2[j][2],define.effect_always_off)
 
                 led.set_bottom_led(define.armor_bottom_all,
-                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_flash)
+                RGB2[j][0],RGB2[j][1],RGB2[j][2],define.effect_flash)
 
                 for i in range(1,9):
                     led.set_single_led(define.armor_top_all,
                     [i],define.effect_always_on)
-                    time.sleep(delay1)
+                    time.sleep(delay2)
                 led.gun_led_off()
 
                 for i in range(1,9):
                     led.set_single_led(define.armor_top_all,
                     [i],define.effect_always_off)
-                    time.sleep(delay1)
+                    time.sleep(delay2)
 
                 if commands_exit==a:continue
                 elif commands_exit==b:break
@@ -614,37 +668,37 @@ def start():
 
             gimbal.rotate(define.gimbal_left)
 
-            for i in range(1,8):
+            for j in range(1,8):
                 commands_exit=random.randint(a,b)
                 gimbal.rotate(define.gimbal_left)
                 led.gun_led_on()
                 led.set_top_led(define.armor_top_all,
-                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_off)
+                RGB2[j][0],RGB2[j][1],RGB2[j][2],define.effect_always_off)
 
                 led.set_bottom_led(define.armor_bottom_all,
-                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_flash)
+                RGB2[j][0],RGB2[j][1],RGB2[j][2],define.effect_flash)
 
                 for i in range(8,0,-1):
                     led.set_single_led(define.armor_top_all,
                     [i],define.effect_always_on)
-                    time.sleep(delay1)
+                    time.sleep(delay2)
                 led.gun_led_off()
 
                 for i in range(8,0,-1):
                     led.set_single_led(define.armor_top_all,
                     [i],define.effect_always_off)
-                    time.sleep(delay1)
+                    time.sleep(delay2)
 
                 if commands_exit==a:continue
                 elif commands_exit==b:break
 
         rgb_colour_trail_chasers_reverse()
 
-# RGB Flash Colour Changers Forward Reverse:
+# RGB Colour Changers Forward Reverse Function:
 
-    def rgb_flash_colour_changers_forward_reverse():      
+    def rgb_colour_changers_forward_reverse():      
 
-        def rgb_flash_colour_changers_forward():
+        def rgb_colour_changers_forward():
             robot.set_mode(define.robot_mode_chassis_follow)
             gimbal.stop()
             
@@ -655,14 +709,14 @@ def start():
 
                 led.set_bottom_led(define.armor_bottom_all,
                 RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_on)
-                time.sleep(second)
+                time.sleep(delay1)
 
                 if commands_exit==a:continue
                 elif commands_exit==b:break
 
-        rgb_flash_colour_changers_forward()
+        rgb_colour_changers_forward()
 
-        def rgb_flash_colour_changers_reverse():
+        def rgb_colour_changers_reverse():
             robot.set_mode(define.robot_mode_chassis_follow)
             gimbal.stop()
             
@@ -673,31 +727,37 @@ def start():
 
                 led.set_bottom_led(define.armor_bottom_all,
                 RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_on)
-                time.sleep(second)
+                time.sleep(delay1)
 
                 if commands_exit==a:continue
                 elif commands_exit==b:break
    
-        rgb_flash_colour_changers_reverse()
+        rgb_colour_changers_reverse()
 
-# Sleep Mode:
+# RGB Flash Colour Changers Function:
+        
+    def rgb_flash_colour_changers():
+        robot.set_mode(define.robot_mode_chassis_follow)
+        gimbal.stop()
 
-    def sleep_mode():
-        robot.set_mode(define.robot_mode_free)
+        for j in range(2):
+            led_ctrl.gun_led_on()
+            for i in range(1,5):
+                led.set_top_led(define.armor_top_all,
+                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_on)
 
-        while True:
-            randdelay=random.randint(1,4)
-            commands_exit=random.randint(a,c)
+                led.set_bottom_led(define.armor_bottom_all,
+                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_on)
+                time.sleep(delay2)
 
-            led.set_top_led(define.armor_top_all,l2,l2,l1,define.effect_breath)
-            led.set_bottom_led(define.armor_bottom_right,l2,l2,l1,define.effect_breath)
-            led.set_bottom_led(define.armor_bottom_left,l2,l2,l1,define.effect_breath)
-            led.set_bottom_led(define.armor_bottom_front,l2,l2,l2,define.effect_breath)
-            led.set_bottom_led(define.armor_bottom_back,l2,l1,l1,define.effect_breath)
-            gimbal.recenter();time.sleep(randdelay)
+            led_ctrl.gun_led_off()
+            for i in range(3,1,-1):
+                led.set_top_led(define.armor_top_all,
+                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_on)
 
-            if commands_exit==a:continue
-            elif commands_exit==b:break
+                led.set_bottom_led(define.armor_bottom_all,
+                RGB2[i][0],RGB2[i][1],RGB2[i][2],define.effect_always_on)
+                time.sleep(delay2)
 
     robomaster_s1_start()
 
@@ -708,14 +768,16 @@ def start():
         if randfunc==1:robot_all_wheel_omni_directional_drive()
         elif randfunc==2:gimbal_free_mode_right_left()
         elif randfunc==3:chassis_follow_gimbal_right_left()
-        elif randfunc==4:blaster_fire()
-        elif randfunc==35:scan_search_right_left()
-        elif randfunc==5:single_led_chassis_follow_gimbal_rotation_right_left()
-        elif randfunc==6:double_led_chassis_follow_gimbal_rotation_right_left()
-        elif randfunc==7:quad_led_gimbal_rotation_up_down()
-        elif randfunc==8:rgb_colour_trail_chasers_forward_reverse()
-        elif randfunc==9:rgb_flash_colour_changers_forward_reverse()
-        elif randfunc==34:sleep_mode()
+        elif randfunc==4:dance_rock()
+        elif randfunc==5:blaster_fire()
+        elif randfunc==34:scan_search_right_left()
+        elif randfunc==35:sleep()
+        elif randfunc==6:single_led_chassis_follow_gimbal_rotation_right_left()
+        elif randfunc==7:double_led_chassis_follow_gimbal_rotation_right_left()
+        elif randfunc==8:quad_led_gimbal_rotation_up_down()
+        elif randfunc==9:rgb_colour_trail_chasers_forward_reverse()
+        elif randfunc==11:rgb_colour_changers_forward_reverse()
+        elif randfunc==12:rgb_flash_colour_changers()
 
 def armor_hit_detection_all(msg):
 
